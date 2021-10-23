9798f66fa6859dd1933aa9e084a1eee3c9ca6e32
Aladár Knís <aladar.knis@gmail.com>
2021-10-23 15:06:00 +0200

    Add project files.

---
+++ YoutubeToMp3.cs
@@ -0,0 +1,28 @@
+﻿using MediaToolkit;
+using MediaToolkit.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoLibrary;
+
+namespace ProjektDjAladar
+{
+    public static class YoutubeToMp3
+    {
+        public static string DownloadVideo(string url)
+        {
+            var source = @"E:\bot\";
+            var youtube = YouTube.Default;
+            var vid = youtube.GetVideo(url);
+            File.WriteAllBytes(source + vid.FullName, vid.GetBytes());
+
+            var file = new MediaFile { Filename = source + vid.FullName };
+            
+
+            return file.Filename.ToString();
+        }
+    }
+}
