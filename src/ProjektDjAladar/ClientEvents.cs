40ce90334aec0f27c6e1076ca29e94ef455334c6
Jakub Kopecky <dede.kopecky@gmail.com>
2021-11-05 10:41:02 +0100

    Program.cs clean up

---
+++ src/ProjektDjAladar/ClientEvents.cs
@@ -0,0 +1,33 @@
+﻿
+
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace ProjektDjAladar
+{
+	class ClientEvents
+	{
+        private readonly EventId BotEventId = new EventId(42, "Bot-Ex04");
+
+        public Task Client_Ready(DiscordClient sender, ReadyEventArgs e)
+        {
+            sender.Logger.LogInformation(BotEventId, "Client is ready to process events.");
+            return Task.CompletedTask;
+        }
+
+        public Task Client_GuildAvailable(DiscordClient sender, GuildCreateEventArgs e)
+        {
+            sender.Logger.LogInformation(BotEventId, $"Guild available: {e.Guild.Name}");
+            return Task.CompletedTask;
+        }
+
+        public Task Client_ClientError(DiscordClient sender, ClientErrorEventArgs e)
+        {
+            sender.Logger.LogError(BotEventId, e.Exception, "Exception occured");
+            return Task.CompletedTask;
+        }
+
+    }
+}
---
+++ src/ProjektDjAladar/CommandEvents.cs
@@ -0,0 +1,36 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Exceptions;
+using DSharpPlus.Entities;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace ProjektDjAladar
+{
+    class CommandEvents
+    {
+        private readonly EventId BotEventId = new EventId(42, "Bot-Ex04");
+
+        public Task Commands_CommandExecuted(CommandsNextExtension sender, CommandExecutionEventArgs e)
+        {
+            e.Context.Client.Logger.LogInformation(BotEventId, $"{e.Context.User.Username} successfully executed '{e.Command.QualifiedName}'");
+            return Task.CompletedTask;
+        }
+
+        public async Task Commands_CommandErrored(CommandsNextExtension sender, CommandErrorEventArgs e)
+        {
+            e.Context.Client.Logger.LogError(BotEventId, $"{e.Context.User.Username} tried executing '{e.Command?.QualifiedName ?? "<unknown command>"}' but it errored: {e.Exception.GetType()}: {e.Exception.Message ?? "<no message>"}", DateTime.Now);
+
+            if (e.Exception is ChecksFailedException ex)
+            {
+                var emoji = DiscordEmoji.FromName(e.Context.Client, ":no_entry:");
+                var embed = new DiscordEmbedBuilder
+                {
+                    Title = "Access denied",
+                    Description = $"{emoji} You do not have the permissions required to execute this command.",
+                    Color = new DiscordColor(0xFF0000)
+                };
+                await e.Context.RespondAsync(embed);
+            }
+        }
+    }
+}
