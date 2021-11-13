8d795e9c5beef9c0834bf4e2300c1fe82c703832
SamoKopecky <sano.kopecky@gmail.com>
2021-11-13 13:39:54 +0100

    Fix shell script

--- WebHookServer/update_docker_container.sh
+++ WebHookServer/update_docker_container.sh
@@ -5,8 +5,7 @@ OLD_BRANCH=`git branch | grep "^\*" | cut -c 3-`
 git stash
 git checkout master
 git pull origin master
+docker-compose up -d --build
 git checkout $OLD_BRANCH
 git stash pop
-docker-compose up -d --build
 
-# Test
