65ffa3c3758f269263cf4ab06b110392ea2b2da7
SamoKopecky <sano.kopecky@gmail.com>
2022-02-12 16:04:34 +0100

    Fix webhook server

--- WebHookServer/main.rb
+++ WebHookServer/main.rb
@@ -2,13 +2,14 @@ require 'sinatra'
 require 'json'
 
 set :bind, '0.0.0.0'
+root_dir = Dir.pwd
 
 post '/payload' do
   push_json = JSON.parse(request.body.read)
   puts "I got some JSON: #{push_json.inspect}"
   ref = push_json["ref"]
   if ref.include? "main" or ref.include? "master"
-    puts `#{Dir.pwd}/update_docker_container.sh >&2`
+    puts `#{root_dir}/update_docker_container.sh >&2`
   end
 end
 
--- WebHookServer/update_docker_container.sh
+++ WebHookServer/update_docker_container.sh
@@ -5,7 +5,7 @@ OLD_BRANCH=`git branch | grep "^\*" | cut -c 3-`
 git stash
 git checkout master
 git pull origin master
-docker-compose up -d --build
+./build.sh
 git checkout $OLD_BRANCH
 git stash pop
 
